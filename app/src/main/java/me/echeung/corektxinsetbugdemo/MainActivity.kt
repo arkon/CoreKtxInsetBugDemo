20ced36664f4e0e01640071a7e57ae780b96e2a1
arkon <eugcheung94@gmail.com>
2021-03-07 23:29:01 -0500

    Add bug demo

--- app/src/main/java/me/echeung/corektxinsetbugdemo/MainActivity.kt
+++ app/src/main/java/me/echeung/corektxinsetbugdemo/MainActivity.kt
@@ -2,10 +2,33 @@ package me.echeung.corektxinsetbugdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Draw edge-to-edge
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        // This doesn't seem to handle the insets for the navigation bar
+        // when using androidx.core:core-ktx:1.5.0-beta02
+        val content = findViewById<FrameLayout>(R.id.contents)
+        ViewCompat.setOnApplyWindowInsetsListener(content) { _, insets ->
+            val systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            content.setPadding(
+                systemInsets.left,
+                systemInsets.top,
+                systemInsets.right,
+                systemInsets.bottom
+            )
+            insets
+        }
     }
 }
